7bfdca452fc64c69e3c4667fc017435659190795
SBDushenkov <papa@localhost.localdomain>
2024-10-22 13:22:01 +0300

    with http2

--- main.sh
+++ main.sh
@@ -1,41 +1,54 @@
 #!/usr/bin/bash
 
+### ПРИ ЛЮБОЙ ОШИБКЕ ПРЕРЫВАЕМ ВЫПОЛНЕНИЕ
 set -e
 
 tmp_dir=$(pwd)
 
-if [ ! -d "$JAVA_TEST_DIR" ]; then
+### ПОДГОТОВКА ПУТЕЙ
+### ЗДЕСЬ ЛЕЖАТ НАШИ СОБСТВЕННЫЕ ТЕСТЫ НА JAVA. ПО УМОЛЧАНИЮ СКРИПТ ЗАПУСКАЕТСЯ УЖЕ ИЗ ДИРЕКТОРИИ С ТЕСТАМИ
+if [ "$JAVA_TEST_DIR" = '' ]; then
     echo "JAVA_TEST_DIR not set"
     JAVA_TEST_DIR=${tmp_dir}
 fi
 echo "JAVA_TEST_DIR=${JAVA_TEST_DIR}"
 
+### ПРОВЕРКА, ЧТО НАШИ СОБСТВЕННЫЕ ТЕСТЫ, ДЕЙСТВИТЕЛЬНО ЛЕЖАТ ПО УКАЗАННОМУ АДРЕСУ
 if [ ! -f "$JAVA_TEST_DIR"/pom.xml ]; then
     echo "pom.xml not found ${JAVA_TEST_DIR} not found. exiting"  1>&2
     exit 1
 fi
 
+### ПО УМОЛЧАНИЮ КОД NGINX ЛЕЖИТ В ДИРЕКТОРИИ source ОТНОСИТЕЛЬНО ТЕКУЩЕЙ
 if [ "$SOURCE_DIR" = '' ]; then
     echo "SOURCE_DIR not set"
     SOURCE_DIR=${tmp_dir}/source
 fi
 echo "SOURCE_DIR=${SOURCE_DIR}"
 
+### ЗДЕСЬ БУДУТ РЕЗУЛЬТАТЫ ВЫПОЛНЕНИЯ СКРИПТА. ПО УМОЛЧАНИЮ ДИРЕКТОРИЯ nginx ОТНОСИТЕЛЬНО ТЕКУЩЕЙ
 if [ "$WORK_DIR" = '' ]; then
     echo "WORK_DIR not set"
     WORK_DIR=${tmp_dir}/nginx
 fi
 echo "WORK_DIR=${WORK_DIR}"
 
+### ЕСЛИ ДИРЕКТОРИИ С РЕЗУЛЬТАТАМИ ВЫПОЛНЕНИЯ СКРИПТА НЕ СУЩЕСТВУЕТ, СОЗДАЕМ ЕЁ
 if [ ! -d "$WORK_DIR" ]; then
     mkdir "${WORK_DIR}"
 fi
 
+### ДИРЕКТОРИЯ ДЛЯ ТЕСТОВОЙ КОМПИЛЯЦИИ NGINX
 coverage_dir="${WORK_DIR:?}"/coverage
+### ДИРЕКТОРИЯ C ОТЧЕТАМИ О ПОКРЫТИИ КОДА
 report_dir="${WORK_DIR:?}"/report
+### ДИРЕКТОРИЯ C ВЫВОДОМ САНИТАЙЗЕРА
 sanitizer_log="${WORK_DIR:?}"/sanitizer-log
+### ДИРЕКТОРИЯ C ОТЧЕТОМ О ВЫПОЛНЕНИИ ТЕСТОВ
 test_result_dir="${WORK_DIR:?}"/test-result
 
+### ОЧИСТКА ДИРЕКТОРИИ С РЕЗУЛЬТАТАМИ ВЫПОЛНЕНИЯ СКРИПТА. НА СЛУЧАЙ, ЕСЛИ ДИРЕКТОРИЯ БЫЛА НЕ ПУСТА
+### ПОДГОТОВКА ДИРЕКТОРИИ ДЛЯ ВЫВОДА РЕЗУЛЬТАТОВ
 rm -rf "${coverage_dir:?}"
 rm -rf "${report_dir:?}"
 rm -rf "${test_result_dir:?}"
@@ -45,12 +58,14 @@ mkdir "${report_dir:?}"
 mkdir "${test_result_dir:?}"
 mkdir "${sanitizer_log:?}"
 
+### ПОРТ ПО УМОЛЧАНИЮ. DEFAULT НЕ ПОДХОДЯТ, Т.К. ТРЕБУЮТ root ПРАВА
 if [ "$DEFAULT_HTTP_PORT" = '' ]; then
   echo "DEFAULT_HTTP_PORT not set"
   DEFAULT_HTTP_PORT=13081
 fi
 echo "DEFAULT_HTTP_PORT=${DEFAULT_HTTP_PORT}"
 
+### ЕСЛИ КОД NGINX ОТСУТСТВУЕТ, СКАЧИВАНИЕ ИЗ GIT
 if [ ! -d "$SOURCE_DIR" ]; then
     echo "source not found. downloading"
     mkdir "$SOURCE_DIR"
@@ -71,6 +86,13 @@ echo "configuring dbg with prefix=${prefix}"
 #--builddir=./target/ должно быть относительным и внутри. нужно разбираться
 
 #sudo apt-get install zlib1g-dev libpcre3 libpcre3-dev libbz2-dev libssl-dev build-essential
+#sudo apt install libssl-dev
+
+### КОНФИГУРИРОВАНИЕ Makefile ДЛЯ СБОРКИ NGINX
+### ОТКЛЮЧЕНА ОПТИМИЗАЦИЯ (ДЛЯ АНАЛИЗА ЛОГОВ)
+### ВКЛЮЧАЕМ ОПЦИИ КОМПИЛЯЦИИ (ПОКРЫТИЕ, САНИТАЙЗЕР)
+### ВКЛЮЧАЕМ НЕОБХОДИМЫЕ ВСТРОЕННЫЕ МОДУЛИ, ИСКЛЮЧАЕМ НЕИСПОЛЬЗУЕМЫЕ*
+### * НУЖНЫЕ МОДУЛИ ВКЛЮЧЕНЫ ПО УМОЛЧАНИЮ
 
 ./auto/configure \
 --with-cc-opt='-O0 --coverage -fsanitize=address -static-libasan -g ' \
@@ -124,8 +146,10 @@ echo "configuring dbg with prefix=${prefix}"
 --without-stream_upstream_random_module \
 --without-stream_upstream_zone_module
 
-echo "configuration dbg done"
+### ДОБАВЛЯЕМ ИНСТРУКЦИИ ЛИНКОВЩИКУ ДЛЯ РАБОТЫ САНИТАЙЗЕРА И ПОКРЫТИЯ КОДА
+### ВЫПОЛНЯЕМ КОМПИЛЯЦИЮ И ИНСТАЛЛИРУЕМ NGINX
 
+echo "configuration dbg done"
 
 echo "patching makefile"
 sed -i 's/\t-Wl,-E/\t-Wl,-E --coverage -fsanitize=address -static-libasan -g/' ./target/Makefile
@@ -133,8 +157,9 @@ sed -i 's/\t-Wl,-E/\t-Wl,-E --coverage -fsanitize=address -static-libasan -g/' .
 echo "installing"
 make -f ./Makefile install
 
+### ГОТОВИМСЯ К ПЕРВОМУ ЗАПУСКУ NGINX. ПРАВИМ ПОРТ В КОНФИГУРАЦИОННОМ ФАЙЛЕ ПО УМОЛЧАНИЮ.
+### ЭТОТ ФАЙЛ НУЖЕН ТОЛЬКО ДЛЯ ЗАПУСАК, В ТЕСТАХ ОН БУДЕТ ДИНАМИЧЕСКИ ЗАМЕНЯТЬСЯ
 echo "patching conf for initial start"
-
 port_mod='s/        listen       80;/        listen       '${DEFAULT_HTTP_PORT}';/'
 sed -i \
   "${port_mod}" \
@@ -146,6 +171,7 @@ pgrep -u "${USER}" nginx | while read -r pid ; do
   kill -9 "${pid}"
 done
 
+### ЗАПУСК NGINX
 # 1. Чтобы отлаживались внешние библиотеки linux нужен su. Нам анализ printf не нужен
 # 2. https://forum.nginx.org/read.php?21,291313,291315#msg-291315
 # Практика есть, но конкретно leak sanitizer бесполезен примерно
@@ -171,6 +197,7 @@ export ASAN_OPTIONS="detect_leaks=0:log_path=./target/nginx:verbosity=1"
     exit 1
 }
 
+### ЗАПУСК ТЕСТОВ
 cd "$JAVA_TEST_DIR"
 mvn clean || result=$(($?))
 
@@ -189,6 +216,7 @@ echo "stopping nginx"
 ./target/nginx -s quit
 mv ./target/log* "${sanitizer_log}"
 
+### ЕСЛИ БЫЛИ ОШИБКИ ВЫПОЛНЕНИЯ ТЕСТОВ, СООБЩАЕМ
 echo "processing results"
 
 if [ "$failures" != "" ]; then
@@ -196,6 +224,7 @@ if [ "$failures" != "" ]; then
     exit 3
 fi
 
+### ФОРМИРОВАНИЕ ОТЧЕТА О ПОКРЫТИИ КОДА
 lcov -t "report" -o report.info -c -d .
 genhtml report.info -o "${report_dir}"
 
